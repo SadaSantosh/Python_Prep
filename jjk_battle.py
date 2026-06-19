4b3cd867ce6fbd7a1a57a0667053d93a4aaf9e4c
SadaSAntosh <sadasantosh29@gmail.com>
2026-06-19 14:55:22 +0530

    Fixed Black flash bug

--- jjk_battle.py
+++ jjk_battle.py
@@ -16,11 +16,18 @@ while fighter_hp > 0 and fighter2_hp > 0:
     fighter2_hp = fighter2_hp - attack_dmage 
     print(f"{fighter_namae} uses Hollow Purple and deals {attack_dmage} damage!! Sukuna HP: {fighter2_hp}")
 
+    if attack_dmage > 20:
+        black_flash = random.randint(1,10)
+        fighter2_hp = fighter2_hp - black_flash
+        print(f"{fighter_namae} has used Black Flash and deals {black_flash} damage!!!! on {fighter2_name} ")
+
     #checks if Sukuna is defeated 
     if fighter2_hp < 0:
         print(f"\n**** {fighter2_name} is defeated!! {fighter_namae} WINS!!!!!!! ***")
         break #this command breakes us out of the loop adn ends theh game 
-
+    if fighter2_hp < 30:
+        fighter2_hp = fighter2_hp + attack_dmage
+        print(f"{fighter2_name} has used reversed cursed technique to heal himself. {fighter2_name} HP: {fighter2_hp}")
     Sukuna_damage = random.randint(15,30)
     fighter_hp = fighter_hp - Sukuna_damage
     print(f"{fighter2_name} uses Cleave and deals {Sukuna_damage} damage! Gojo HP: {fighter_hp}\n")
