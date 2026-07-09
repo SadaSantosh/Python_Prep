9e6eb4785682c78145c7a1a8798797f53005fa71
SadaSAntosh <sadasantosh29@gmail.com>
2026-07-09 21:55:16 +0530

    Day 3: Implemented Multi-line comparison plots and legends

--- data_viz.py
+++ data_viz.py
@@ -6,14 +6,17 @@ print("____________________ Day 3:Data Visualization Intro __________________")
 # 1. workout progression dataset 
 workout_data = {
     "week": [1, 2, 3, 4, 5, 6, 7],
-    "Max_Pull_Ups": [4, 5, 6, 7, 8, 9, 11]
+    "Max_Pull_Ups": [4, 5, 6, 7, 8, 9, 11],
+    "Yuji_Max_Pull_Ups": [3, 3, 5, 5, 8, 8, 10] #new comarison column added to the dataset
 }
 
 # 2. Convert to DataFrame
 df = pd.DataFrame(workout_data)
 
 # 3. Create a clean line plot graph to visualize the progression of pull-ups over the weeks
-plt.bar(df["week"], df["Max_Pull_Ups"], color='crimson') # bar graph with crimson color
+plt.plot(df["week"], df["Max_Pull_Ups"], marker='o', color='crimson', label='Your Progress ', linewidth=2) # bar graph with crimson color
+plt.plot(df["week"], df["Yuji_Max_Pull_Ups"], marker='s', color='royalblue', label='Yuji Progress', linewidth=2)
+plt.legend() # adds a legend to the graph to identify the two lines
 
 # 4. Add professional labels and title to the graph
 plt.title("Calisthenics Progression: Max Pull-Ups Over Time")
